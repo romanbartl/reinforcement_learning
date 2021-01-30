3038e58f4dd883c7ef4ed1c0e349ed1eb7d30fd5
Roman Bartl <bbartl.roman@gmail.com>
2021-01-30 18:43:36 +0100

    Ball moving

--- Ball.py
+++ Ball.py
@@ -2,6 +2,7 @@ import pygame
 
 DEFAULT_DIAMETER = 16
 DEFAULT_COLOR = (0, 128, 0)
+DEFAULT_STEP = 0.5
 
 class Ball(object):
     def __init__(self, screen_size):
@@ -9,7 +10,9 @@ class Ball(object):
         self.diameter = DEFAULT_DIAMETER
         self.x = (self.screen_size[0] - self.diameter) / 2
         self.y = self.screen_size[1] - self.diameter - 40
+        self.step = DEFAULT_STEP
 
     def draw(self, screen):
+        if self.y != 0:
+            self.y = self.y - self.step
         pygame.draw.circle(screen, DEFAULT_COLOR, (self.x, self.y), self.diameter)
-    
