6850aad1f4ac6361ab9bb5f47fca51959484fbff
Roman Bartl <bbartl.roman@gmail.com>
2021-01-31 14:34:25 +0100

    Environment class for agent

---
+++ Actions.py
@@ -0,0 +1,5 @@
+from enum import Enum 
+
+class Actions(Enum):
+    LEFT = 0
+    RIGHT = 1
---
+++ Environment.py
@@ -0,0 +1,19 @@
+import pygame
+from GameState import GameState
+from Player import Player
+from Ball import Ball
+from Actions import Actions
+
+
+class Environment:
+    def __init__(self):
+        pass
+
+
+    def step(self, action):
+        pass
+    
+
+    def render_scene(self):
+        pass
+    
